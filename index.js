4bca760f4b165dc66de6057f9ea9f4ae40773927
justinmchase <justin.m.chase@gmail.com>
2015-05-11 08:07:27 -0500

    Add to the sample apps ul

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 console.log('loading electron-updater-sample-plugin...')
 
 function plugin(context) {
-	console.log('loaded electron-updater-sample-plugin!')
+	var d = context.document
+	var ul = d.getElementById('plugins')
+	var li = d.createElement('li')
+	li.innerHTML = 'electron-updater-sample-plugin'
+	ul.appendChild(li)
 }
 
 module.exports = plugin
